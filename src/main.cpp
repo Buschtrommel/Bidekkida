bda222f84c95115310c3d6e6dbdaeef5d12591a5
Buschmann <kontakt@buschmann23.de>
2017-09-25 09:14:11 +0200

    Read default priority from configuration file

--- src/main.cpp
+++ src/main.cpp
@@ -59,7 +59,7 @@ int main(int argc, char *argv[])
     const QString regex = settings->value(QStringLiteral("regex"), QStringLiteral("^([a-f0-9:\\.]{3,39})\\s.*")).toString();
 
     const QString _backend = settings->value(QStringLiteral("backend"), QStringLiteral("file")).toString();
-        Anonymizer::Backend backend;
+    Anonymizer::Backend backend = Anonymizer::File;
     if (_backend == QLatin1String("syslog")) {
         backend = Anonymizer::Syslog;
 #ifdef WITH_SYSTEMD
@@ -73,6 +73,26 @@ int main(int argc, char *argv[])
     const QString identifier = settings->value(QStringLiteral("identifier"), QCoreApplication::applicationName()).toString();
     const bool anonymizeIp = settings->value(QStringLiteral("anonymize"), true).toBool();
 
+    const QString _priority = settings->value(QStringLiteral("priority"), QStringLiteral("6")).toString();
+    Anonymizer::Priority priority = Anonymizer::Informational;
+    if ((_priority == QLatin1String("0")) || (QString::compare(_priority, QLatin1String("emergency"), Qt::CaseInsensitive) == 0)) {
+        priority = Anonymizer::Emergency;
+    } else if ((_priority == QLatin1String("1")) || (QString::compare(_priority, QLatin1String("alert"), Qt::CaseInsensitive) == 0)) {
+        priority = Anonymizer::Alert;
+    } else if ((_priority == QLatin1String("2")) || (QString::compare(_priority, QLatin1String("critical"), Qt::CaseInsensitive) == 0)) {
+        priority = Anonymizer::Critical;
+    } else if ((_priority == QLatin1String("3")) || (QString::compare(_priority, QLatin1String("error"), Qt::CaseInsensitive) == 0)) {
+        priority = Anonymizer::Error;
+    } else if ((_priority == QLatin1String("4")) || (QString::compare(_priority, QLatin1String("warning"), Qt::CaseInsensitive) == 0)) {
+        priority = Anonymizer::Warning;
+    } else if ((_priority == QLatin1String("5")) || (QString::compare(_priority, QLatin1String("notice"), Qt::CaseInsensitive) == 0)) {
+        priority = Anonymizer::Notice;
+    } else if ((_priority == QLatin1String("6")) || (QString::compare(_priority, QLatin1String("informational"), Qt::CaseInsensitive) == 0)) {
+        priority = Anonymizer::Informational;
+    } else {
+        priority = Anonymizer::Debug;
+    }
+
     settings->endGroup();
 
     delete settings;
@@ -81,6 +101,7 @@ int main(int argc, char *argv[])
     anon.setBackend(backend);
     anon.setIdentifier(identifier);
     anon.setAnonymizeIp(anonymizeIp);
+    anon.setPriority(priority);
     if (!anon.run()) {
         return 2;
     }
